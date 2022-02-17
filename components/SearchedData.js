f5954b90eb1dfe445593c36e7250752b5de1d523
pawan67 <pawantamada8@gmail.com>
2022-02-17 12:52:26 +0530

    added key

--- components/SearchedData.js
+++ components/SearchedData.js
@@ -4,9 +4,10 @@ function SearchedData({ data }) {
   console.log(data);
   return (
     <div className="  2xl:right-64 max-h-[600px] overflow-y-scroll z-50 absolute right-5 bg-white top-16 rounded-xl shadow-2xl w-[300px] md:w-[400px]  ">
-      {data.map((items) => {
+      {data.map((items, id) => {
+        
         return (
-          <div className=" p-5 hover:bg-gray-100 flex  justify-between items-center">
+          <div key={id} className=" p-5 hover:bg-gray-100 flex  justify-between items-center">
             <div className="  min-w-xs max-w-sm">
               <div>{items.title}</div>
               <p className=" text-sm text-gray-600">{items.source}</p>
